ceee8d566a51cd9718a97797370c3243ede975e8
Asane <yaradayo317@gmail.com>
2022-02-13 16:37:47 -0500

    added getStaticProps function to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function HomePage() {
+export default function HomePage({ flashcards }) {
   return (
     <Layout title="Home Page | Flashcard">
-      <h1>Home Page</h1>
+      <h1>Words</h1>
     </Layout>
   );
+}
+
+// get running everytime you come to the page
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/flashcards`);
+  const flashcards = await res.json();
+
+  return {
+    props: { flashcards },
+    revalidate: 1,
+  };
 }
