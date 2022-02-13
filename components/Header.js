727cd9c86bfdfc8c9954474c05dea2dde3ae2301
Asane <yaradayo317@gmail.com>
2022-02-13 16:00:26 -0500

    created footer component

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import styles from "@/styles/Header.module.css";
 
 export default function Header() {
   return (
-    <header className="styles.header">
+    <header className={styles.header}>
       <div className={styles.logo}>
         <Link href="/">
           <a>Flashcard</a>
