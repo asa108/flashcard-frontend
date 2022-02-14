f7c06f10faf0beccd944b778f6ef26d5fc9a65f8
Asane <yaradayo317@gmail.com>
2022-02-14 16:10:51 -0500

    show add flashcard if user login

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import Link from "next/link";
+import AuthContext from "@/context/AuthContext";
 import styles from "@/styles/Header.module.css";
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -18,18 +22,34 @@ export default function Header() {
               <a>Flashcard</a>
             </Link>
           </li>
-          <li>
-            <Link href="/flashcards/add">
-              <a>Add flashcard</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/account/login">
-              <a className="btn-secondary">
-                <FaSignInAlt /> Login
-              </a>
-            </Link>
-          </li>
+          {user ? (
+            // If logged in
+            <>
+              <li>
+                <Link href="/flashcards/add">
+                  <a>Add flashcard</a>
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={() => logout()}
+                  className="btn-secondary btn-icon"
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href="/account/login">
+                  <a className="btn-secondary">
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
