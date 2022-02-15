f3462776d487b015c8b506b89db20aee97fff3d2
Asane <yaradayo317@gmail.com>
2022-02-14 20:48:51 -0500

    parse logged in user

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -8,6 +8,10 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
+  const router = useRouter();
+
+  useEffect(() => checkUserLoggedIn(), []);
+
   // Register user
   const register = async (user) => {
     console.log(user);
@@ -30,6 +34,7 @@ export const AuthProvider = ({ children }) => {
     console.log(data);
     if (res.ok) {
       setUser(data.user);
+      router.push("/flashcards");
     } else {
       setError(data.message);
       setError(null);
@@ -43,7 +48,13 @@ export const AuthProvider = ({ children }) => {
 
   // Check if user login
   const checkUserLoggedIn = async (user) => {
-    console.log("Check");
+    const res = await fetch(`${NEXT_URL}/api/user`);
+    const data = await res.json();
+    if (res.ok) {
+      setUser(data.user);
+    } else {
+      setUser(null);
+    }
   };
 
   return (
---
+++ pages/account/dashboard.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/Layout";
+
+export default function DashboardPage() {
+  return (
+    <Layout title="User Dashboard">
+      <h1>Dashbord</h1>
+    </Layout>
+  );
+}
