050f94cbb0ee1972703862e3305f012221a95aaf
Asane <yaradayo317@gmail.com>
2022-02-15 17:26:21 -0500

    get user flashcards for dashboard page

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,30 @@
+import { parseCookies } from "@/helpers/index";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function DashboardPage() {
+export default function DashboardPage({ flashcards }) {
+  console.log(flashcards);
   return (
     <Layout title="User Dashboard">
       <h1>Dashbord</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  console.log(token);
+  const res = await fetch(`${API_URL}/flashcards/me`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const flashcards = await res.json();
+
+  return {
+    props: { flashcards },
+  };
 }
