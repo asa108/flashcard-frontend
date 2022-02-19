22dd9ed4936615b91de10ad54c94e162bbf7653c
Asane <yaradayo317@gmail.com>
2022-02-19 11:03:41 -0500

    delete comment out

--- pages/flashcards/index.js
+++ pages/flashcards/index.js
@@ -1,30 +1,3 @@
-// import Layout from "@/components/Layout";
-// import { API_URL } from "@/config/index";
-// import FlashcardList from "@/components/FlashcardList";
-
-// export default function HomePage({ flashcards }) {
-//   return (
-//     <Layout title="Home Page | Flashcard">
-//       <h1>Words</h1>
-//       {flashcards.length === 0 && <h3>No Words</h3>}
-//       <FlashcardList flashcards={flashcards} />
-//     </Layout>
-//   );
-// }
-
-// // get running everytime you come to the page
-// export async function getStaticProps() {
-//   const res = await fetch(`${API_URL}/flashcards`);
-//   // const res = await fetch(`${API_URL}/flashcards?_sort=date:ASC&_limit=3`);
-//   const flashcards = await res.json();
-
-//   return {
-//     props: { flashcards },
-//     // props: { flashcards.slice(0,3) },
-//     revalidate: 1,
-//   };
-// }
-
 import { useEffect,useState } from 'react'
 import Link from "next/link";
 import { parseCookies } from "@/helpers/index";
