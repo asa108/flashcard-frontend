91209decc72254145bd5a03655e06383a6933d5f
Asane <yaradayo317@gmail.com>
2022-02-23 16:01:06 -0500

    changed router

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <Link href="/">
+        <Link href="/flashcards">
           <a>Flashcard</a>
         </Link>
       </div>
